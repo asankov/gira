8b9d3973ba5418642f81d091f4d2346093226ccf
asankov <a.sankov96@gmail.com>
2020-03-23 23:30:27 +0200

    Add `main.go` with a signle route
    that returns empty data.
    
    Add `go.mod` and `go.sum` files.
    Add `github.com/gorilla/mux` as a dependency.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/asankov/gira
+
+go 1.13
+
+require github.com/gorilla/mux v1.7.4
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/games", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("{}"))
+	})
+
+	log.Println("listening on port 4000")
+	if err := http.ListenAndServe(":4000", r); err != nil {
+		log.Fatalf("error while serving: %v", err)
+	}
+}
