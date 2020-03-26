d0a069dc69736dfd24a7bdebcb564dfc3ea296b7
asankov <a.sankov96@gmail.com>
2020-03-26 19:08:41 +0200

    Refactoring

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,14 +5,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/asankov/gira/pkg/models/postgres"
 
 	// to register PostreSQL driver
 	_ "github.com/lib/pq"
 )
 
 type server struct {
-	log     log.Logger
-	handler http.Handler
+	log       *log.Logger
+	handler   http.Handler
+	gameModel *postgres.GameModel
 }
 
 func main() {
@@ -22,16 +26,17 @@ func main() {
 }
 
 func run() error {
-	s := &server{}
 
-	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable", "localhost", 5432, "antonsankov", "gira"))
+	db, err := openDB("localhost", 5432, "antonsankov", "gira")
 	if err != nil {
-		return fmt.Errorf("error while opening connection to db: %w", err)
-	}
-	if err := db.Ping(); err != nil {
-		return fmt.Errorf("error while pinging db: %w", err)
+		return fmt.Errorf("error while opening DB: %w", err)
 	}
 	defer db.Close()
+
+	s := &server{
+		log:       log.New(os.Stdout, "", log.Ldate|log.Ltime),
+		gameModel: &postgres.GameModel{DB: db},
+	}
 
 	log.Println("listening on port 4000")
 	if err := http.ListenAndServe(":4000", s.routes()); err != nil {
@@ -39,4 +44,16 @@ func run() error {
 	}
 
 	return nil
+}
+
+func openDB(host string, port int, user string, dbName string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable", host, port, user, dbName))
+	if err != nil {
+		return nil, fmt.Errorf("error while opening connection to db: %w", err)
+	}
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("error while pinging db: %w", err)
+	}
+
+	return db, nil
 }
