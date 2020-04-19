364878e5a90bdf316fb3fce87409a0a703ed3b5e
asankov <a.sankov96@gmail.com>
2020-04-19 21:26:52 +0300

    Use `justinac/alice` to make composable middlewares

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"github.com/justinas/alice"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 func (s *server) routes() http.Handler {
+	standartMiddleware := alice.New(s.recoverPanic, s.logRequest, s.secureHeaders)
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/games", s.getGamesHandler()).Methods(http.MethodGet)
 	r.HandleFunc("/games/{id}", s.getGameByIDHandler()).Methods(http.MethodGet)
 	r.HandleFunc("/games", s.createGameHandler()).Methods(http.MethodPost)
 
-	return s.recoverPanic(s.logRequest(s.secureHeaders(r)))
+	return standartMiddleware.Then(r)
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/gorilla/mux v1.7.4
+	github.com/justinas/alice v1.2.0
 	github.com/lib/pq v1.3.0
 )
