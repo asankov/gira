301b45c242665fda3a47f8978c21fd765ad72f3f
asankov <a.sankov96@gmail.com>
2020-04-20 02:14:45 +0300

    Add /games view
    with hardcoded data

--- cmd/front-end/routes.go
+++ cmd/front-end/routes.go
@@ -7,11 +7,35 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type game struct {
+	ID   string
+	Name string
+}
+
+type gamesData struct {
+	Games []game
+	// TODO: proper data type
+	User  string
+}
+
 func (s *server) routes() http.Handler {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		s.renderTemplate(w, r, "./ui/html/home.page.tmpl", "./ui/html/base.layout.tmpl")
+		s.renderTemplate(w, r, nil, "./ui/html/home.page.tmpl", "./ui/html/base.layout.tmpl")
+	}).Methods(http.MethodGet)
+
+	// TODO: fetch this from the back-end, instead of hardcoding them
+	data := &gamesData{
+		Games: []game{
+			{ID: "1", Name: "AC"},
+			{ID: "2", Name: "ACII"},
+			{ID: "3", Name: "ACII: Brotherhood"},
+			{ID: "4", Name: "ACII: Liberation"},
+		},
+	}
+	r.HandleFunc("/games", func(w http.ResponseWriter, r *http.Request) {
+		s.renderTemplate(w, r, data, "./ui/html/list.page.tmpl", "./ui/html/base.layout.tmpl")
 	}).Methods(http.MethodGet)
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
@@ -20,7 +44,7 @@ func (s *server) routes() http.Handler {
 	return r
 }
 
-func (s *server) renderTemplate(w http.ResponseWriter, r *http.Request, templates ...string) {
+func (s *server) renderTemplate(w http.ResponseWriter, r *http.Request, data interface{}, templates ...string) {
 	t, err := template.ParseFiles(templates...)
 	if err != nil {
 		s.log.Println(err.Error())
@@ -28,7 +52,7 @@ func (s *server) renderTemplate(w http.ResponseWriter, r *http.Request, template
 		return
 	}
 
-	if err := t.Execute(w, nil); err != nil {
+	if err := t.Execute(w, data); err != nil {
 		s.log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 	}
