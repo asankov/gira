38708542aa117197abcb6085120eb57e5b1286cc
asankov <a.sankov96@gmail.com>
2020-04-19 19:13:52 +0300

    Log onto some parts of the request

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -5,13 +5,13 @@ import "net/http"
 func (s *server) logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// TODO: better format for the request
-		s.log.Printf("Incoming request: %#v\n", r)
+		s.log.Printf("%s - %s %s %s\n", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
 
 		next.ServeHTTP(w, r)
 	})
 }
 
-func (s * server) secureHeaders(next http.Handler) http.Handler {
+func (s *server) secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-XSS-Protection", "1; mode-block")
 		w.Header().Set("X-Frame-Options", "deny")
