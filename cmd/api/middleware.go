b262ee8d9d335b804143c97b3c1349ec85487557
asankov <a.sankov96@gmail.com>
2020-04-19 19:00:00 +0300

    Add `securityHeaders` middleware

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -10,3 +10,12 @@ func (s *server) loggingMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (s * server) secureHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-XSS-Protection", "1; mode-block")
+		w.Header().Set("X-Frame-Options", "deny")
+
+		next.ServeHTTP(w, r)
+	})
+}
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,5 +13,5 @@ func (s *server) routes() http.Handler {
 	r.HandleFunc("/games/{id}", s.getGameByIDHandler()).Methods(http.MethodGet)
 	r.HandleFunc("/games", s.createGameHandler()).Methods(http.MethodPost)
 
-	return r
+	return s.secureHeaders(r)
 }
