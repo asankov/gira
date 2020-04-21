4e8a118e963b48b87490058e392e6d4c484f7fde
asankov <a.sankov96@gmail.com>
2020-04-21 21:04:51 +0300

    Wrap the main function of cmd/front-end into `func run() error`

--- cmd/front-end/main.go
+++ cmd/front-end/main.go
@@ -20,6 +20,12 @@ type server struct {
 }
 
 func main() {
+	if err := run(); err != nil {
+		log.Panic("error while running front-end service: " + err.Error())
+	}
+}
+
+func run() error {
 	port := *flag.Int("port", 4001, "port on which the application is exposed")
 	backEndAddr := *flag.String("api_addr", "http://localhost:4000", "the address to the API service")
 	sessionSecret := *flag.String("session_secret", "s6Ndh+pPbnzHb7*297k1q5W0Tzbpa@ge", "32-byte secret that is to be used for the session store")
@@ -30,7 +36,7 @@ func main() {
 
 	cl, err := client.New(backEndAddr)
 	if err != nil {
-		log.Fatalf("error while creating back-end client: %v", err)
+		return fmt.Errorf("error while creating back-end client: %w", err)
 	}
 
 	s := &server{
@@ -41,6 +47,8 @@ func main() {
 
 	s.log.Println(fmt.Sprintf("Front-end listening on port %d", port))
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), s.routes()); err != nil {
-		log.Fatalf("error while listening: %v", err)
+		return fmt.Errorf("error while listening: %w", err)
 	}
+
+	return nil
 }
