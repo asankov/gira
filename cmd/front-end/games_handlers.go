de2d57fad942efba08b41a44a132cbc41cabd1aa
asankov <a.sankov96@gmail.com>
2020-05-03 18:17:30 +0300

    Split `front-end/handlers.go`
    into `users_handlers.go` and `games_handlers.go`

--- cmd/front-end/games_handlers.go
+++ cmd/front-end/games_handlers.go
@@ -58,34 +58,6 @@ func (s *server) createGameHandler() http.HandlerFunc {
 	}
 }
 
-func (s *server) getSignupFormHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		s.renderTemplate(w, r, nil, "./ui/html/signup.page.tmpl", "./ui/html/base.layout.tmpl")
-	}
-}
-
-func (s *server) createUserHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		username, email, password := r.PostFormValue("username"), r.PostFormValue("email"), r.PostFormValue("password")
-
-		if _, err := s.client.CreateUser(&models.User{
-			Username: username,
-			Email:    email,
-			Password: password,
-		}); err != nil {
-			s.log.Printf("error while creating user: %v", err)
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		// TODO: this is not shown
-		s.session.Put(r, "flash", "User created succesfully.")
-
-		w.Header().Add("Location", "/")
-		w.WriteHeader(http.StatusSeeOther)
-	}
-}
-
 func (s *server) renderTemplate(w http.ResponseWriter, r *http.Request, data interface{}, templates ...string) {
 	t, err := template.ParseFiles(templates...)
 	if err != nil {
