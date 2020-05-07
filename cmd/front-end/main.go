de1414d2718940b9f57cbb76cd75e01318464f5a
asankov <a.sankov96@gmail.com>
2020-05-07 03:41:25 +0300

    Fix bug when flags are ignored.
    Because I am assigning the values before they are initialized by
    `flag.Parse()`.
    
    Instead, just assign the addresses and reference them before using.

--- cmd/front-end/main.go
+++ cmd/front-end/main.go
@@ -20,15 +20,15 @@ func main() {
 }
 
 func run() error {
-	port := *flag.Int("port", 4001, "port on which the application is exposed")
-	backEndAddr := *flag.String("api_addr", "http://localhost:4000", "the address to the API service")
-	sessionSecret := *flag.String("session_secret", "s6Ndh+pPbnzHb7*297k1q5W0Tzbpa@ge", "32-byte secret that is to be used for the session store")
+	port := flag.Int("port", 4001, "port on which the application is exposed")
+	backEndAddr := flag.String("api_addr", "http://localhost:4000", "the address to the API service")
+	sessionSecret := flag.String("session_secret", "s6Ndh+pPbnzHb7*297k1q5W0Tzbpa@ge", "32-byte secret that is to be used for the session store")
 	flag.Parse()
 
-	session := sessions.New([]byte(sessionSecret))
+	session := sessions.New([]byte(*sessionSecret))
 	session.Lifetime = 12 * time.Hour
 
-	cl, err := client.New(backEndAddr)
+	cl, err := client.New(*backEndAddr)
 	if err != nil {
 		return fmt.Errorf("error while creating back-end client: %w", err)
 	}
@@ -39,8 +39,8 @@ func run() error {
 		session: session,
 	}
 
-	s.log.Println(fmt.Sprintf("Front-end listening on port %d", port))
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), s); err != nil {
+	s.log.Println(fmt.Sprintf("Front-end listening on port %d", *port))
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), s); err != nil {
 		return fmt.Errorf("error while listening: %w", err)
 	}
 
