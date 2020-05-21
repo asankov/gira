6674473421976a52f5de594743e1012f42449d18
asankov <a.sankov96@gmail.com>
2020-05-22 02:31:49 +0300

    Generate mock for authenticator

--- internal/fixtures/generated_fixtures.go
+++ internal/fixtures/generated_fixtures.go
@@ -2,3 +2,4 @@ package fixtures
 
 //go:generate mockgen -destination gamemodelmock.go  -package fixtures -mock_names GameModel=GameModelMock github.com/asankov/gira/cmd/api/server GameModel
 //go:generate mockgen -destination usermodelmock.go  -package fixtures -mock_names UserModel=UserModelMock github.com/asankov/gira/cmd/api/server UserModel
+//go:generate mockgen -destination authenticatormock.go  -package fixtures -mock_names Authenticator=AuthenticatorMock github.com/asankov/gira/cmd/api/server Authenticator
