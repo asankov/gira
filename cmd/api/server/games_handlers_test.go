d9d70f75ceaf56546d2a840c1a45ea89286a9b70
asankov <a.sankov96@gmail.com>
2020-05-21 21:28:09 +0300

    More tests for games
    cleanup setup a bit

--- cmd/api/server/games_handlers_test.go
+++ cmd/api/server/games_handlers_test.go
@@ -21,15 +21,18 @@ var (
 	user          = &models.User{Username: "anton"}
 )
 
+func newServer(g GameModel, a *auth.Authenticator) *Server {
+	return &Server{
+		Log:       log.New(os.Stdout, "", 0),
+		GameModel: g,
+		Auth:      a,
+	}
+}
 func TestGetGames(t *testing.T) {
 	ctrl := gomock.NewController(t)
 
 	gameModel := fixtures.NewGameModelMock(ctrl)
-	srv := Server{
-		Log:       log.New(os.Stdout, "", 0),
-		GameModel: gameModel,
-		Auth:      authenticator,
-	}
+	srv := newServer(gameModel, authenticator)
 
 	gamesResponse := []*models.Game{
 		{ID: "1", Name: "AC"},
@@ -76,11 +79,7 @@ func TestGetGamesErr(t *testing.T) {
 	ctrl := gomock.NewController(t)
 
 	gameModel := fixtures.NewGameModelMock(ctrl)
-	srv := Server{
-		Log:       log.New(os.Stdout, "", 0),
-		GameModel: gameModel,
-		Auth:      authenticator,
-	}
+	srv := newServer(gameModel, authenticator)
 
 	gameModel.
 		EXPECT().
@@ -106,11 +105,7 @@ func TestCreateGame(t *testing.T) {
 	ctrl := gomock.NewController(t)
 
 	gameModel := fixtures.NewGameModelMock(ctrl)
-	srv := Server{
-		Log:       log.New(os.Stdout, "", 0),
-		GameModel: gameModel,
-		Auth:      authenticator,
-	}
+	srv := newServer(gameModel, authenticator)
 
 	actualName := "ACIII"
 	actualGame := &models.Game{Name: actualName}
@@ -137,6 +132,52 @@ func TestCreateGame(t *testing.T) {
 	decode(t, w, &game)
 	if game.Name != actualName {
 		t.Fatalf("Got (%s) for game.Name, expected (%s)", game.Name, actualName)
+	}
+}
+
+func TestCreateGameError(t *testing.T) {
+
+	cases := []struct {
+		name string
+		game *models.Game
+	}{
+		{
+			name: "Empty game",
+			game: &models.Game{Name: ""},
+		},
+		{
+			name: "Filled ID",
+			game: &models.Game{ID: "123", Name: "something valid"},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			gameModel := fixtures.NewGameModelMock(ctrl)
+			srv := newServer(gameModel, authenticator)
+
+			actualGame := c.game
+			gameModel.
+				EXPECT().
+				Insert(actualGame).
+				Return(actualGame, nil)
+
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodPost, "/games", marshall(t, actualGame))
+			token, err := srv.Auth.NewTokenForUser(user)
+			if err != nil {
+				t.Fatalf("Got unexpected error while trying to generate token for user - %v", err)
+			}
+			r.Header.Set("x-auth-token", token)
+			srv.ServeHTTP(w, r)
+
+			got, expected := w.Result().StatusCode, http.StatusBadRequest
+			if got != expected {
+				t.Fatalf("Got (%d) for status code, expected (%d)", got, expected)
+			}
+		})
 	}
 }
 
