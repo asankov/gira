28eabe43113ececa5043529f46203f9bca78719c
asankov <a.sankov96@gmail.com>
2020-05-24 04:26:16 +0300

    Some docs

--- cmd/front-end/server/render_template.go
+++ cmd/front-end/server/render_template.go
@@ -3,12 +3,12 @@ package server
 import (
 	"net/http"
 	"text/template"
-
-	"github.com/asankov/gira/pkg/models"
 )
 
+// TemplateRenderer implements Renderer and is used to render templates
 type TemplateRenderer struct{}
 
+// Render implements Renderer
 func (t *TemplateRenderer) Render(w http.ResponseWriter, r *http.Request, d interface{}, p Page) error {
 	tt, err := template.ParseFiles("./ui/html/"+string(p), "./ui/html/base.layout.tmpl")
 	if err != nil {
--- cmd/front-end/server/server.go
+++ cmd/front-end/server/server.go
@@ -8,6 +8,7 @@ import (
 	"github.com/golangcollege/sessions"
 )
 
+// Page represents a Page to be rendered
 type Page string
 
 var (
@@ -18,9 +19,14 @@ var (
 	loginUserPage  Page = "login.page.tmpl"
 )
 
+// Renderer is the interface that will be used to interact with the part of the program
+// that is responsible for rendering the web pages
 type Renderer interface {
 	Render(w http.ResponseWriter, r *http.Request, d interface{}, p Page) error
 }
+
+// Server is the struct that holds all the dependencies
+// needed to run the application
 type Server struct {
 	Log      *log.Logger
 	Session  *sessions.Session
