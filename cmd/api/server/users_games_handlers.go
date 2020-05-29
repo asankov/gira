783179c25a59a41e60559c90e4be5e4fde6426f8
asankov <a.sankov96@gmail.com>
2020-05-29 21:31:00 +0300

    Improvements

--- cmd/api/server/users_games_handlers.go
+++ cmd/api/server/users_games_handlers.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/asankov/gira/pkg/models"
@@ -9,17 +10,9 @@ import (
 
 func (s *Server) handleUsersGamesGet() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		args := mux.Vars(r)
-		id := args["id"]
-		if id == "" {
-			http.Error(w, "Bad Request", http.StatusBadRequest)
-			return
-		}
-
-		user := userFromRequest(r)
-
-		if user.ID != id {
-			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		user, err := userFromRequest(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
@@ -34,6 +27,24 @@ func (s *Server) handleUsersGamesGet() http.HandlerFunc {
 	}
 }
 
-func userFromRequest(r *http.Request) *models.User {
-	return r.Context().Value(userKey).(*models.User)
+func userFromRequest(r *http.Request) (*models.User, error) {
+	args := mux.Vars(r)
+	id := args["id"]
+	if id == "" {
+		return nil, fmt.Errorf(`No 'id' in request`)
+	}
+	usr := r.Context().Value(userKey)
+	if usr == nil {
+		return nil, fmt.Errorf("No user found in request")
+	}
+	user, ok := usr.(*models.User)
+	if !ok {
+		return nil, fmt.Errorf("No user found in request")
+	}
+
+	if user.ID != id {
+		return nil, fmt.Errorf("ID param differs from userID in token")
+	}
+
+	return user, nil
 }
