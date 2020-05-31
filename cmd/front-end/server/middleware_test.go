0cc194409e0e3929045256be641379a72d857b23
asankov <a.sankov96@gmail.com>
2020-05-31 19:19:18 +0300

    Add tests form front-end/server.requireLogin middleware

--- cmd/front-end/server/middleware_test.go
+++ cmd/front-end/server/middleware_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 )
 
+var tokenValue = "my_token"
+
 func TestSecureHeaders(t *testing.T) {
 	srv := &Server{}
 
@@ -23,4 +25,59 @@ func TestSecureHeaders(t *testing.T) {
 
 	r := httptest.NewRequest(http.MethodGet, "/", nil)
 	h.ServeHTTP(httptest.NewRecorder(), r)
+}
+
+func TestRequireLogin(t *testing.T) {
+	srv := &Server{}
+
+	called := false
+	h := srv.requireLogin(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		called = true
+
+		token, ok := r.Context().Value(contextTokenKey).(string)
+		if !ok {
+			t.Errorf("Expected `r.Context().Value(contextTokenKey)` to be of type string")
+		}
+		if token != tokenValue {
+			t.Errorf("Got (%s) for token from context, expected (%s)", token, tokenValue)
+		}
+	}))
+
+	r := httptest.NewRequest(http.MethodGet, "/", nil)
+	r.AddCookie(&http.Cookie{
+		Name:  "token",
+		Value: tokenValue,
+	})
+	h.ServeHTTP(httptest.NewRecorder(), r)
+
+	if !called {
+		t.Errorf("Expected next handler to be called, and `called` to be equal to true, instead `called` is false")
+	}
+}
+
+func TestRequireLoginNoUser(t *testing.T) {
+	srv := &Server{}
+
+	called := false
+	h := srv.requireLogin(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		called = true
+	}))
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodGet, "/", nil)
+	h.ServeHTTP(w, r)
+
+	if called {
+		t.Errorf("Expected next handler to not be called, and `called` to be equal to false, instead `called` is true")
+	}
+
+	got, expected := w.Code, http.StatusSeeOther
+	if got != expected {
+		t.Errorf("Got status code (%d), expected (%d)", got, expected)
+	}
+
+	gotH, expectedH := w.Header().Get("Location"), "/users/login"
+	if gotH != expectedH {
+		t.Errorf("Got (%s) for Location header, expected (%s)", gotH, expectedH)
+	}
 }
