201552b22e675f3f98d0a790b974937dfd761d40
asankov <a.sankov96@gmail.com>
2020-05-31 19:58:46 +0300

    Generate mocks for UserGamesModel

--- internal/fixtures/generated_fixtures.go
+++ internal/fixtures/generated_fixtures.go
@@ -2,5 +2,6 @@ package fixtures
 
 //go:generate mockgen -destination gamemodelmock.go  -package fixtures -mock_names GameModel=GameModelMock github.com/asankov/gira/cmd/api/server GameModel
 //go:generate mockgen -destination usermodelmock.go  -package fixtures -mock_names UserModel=UserModelMock github.com/asankov/gira/cmd/api/server UserModel
+//go:generate mockgen -destination user_games_model_mock.go  -package fixtures -mock_names UserGamesModel=UserGamesModelMock github.com/asankov/gira/cmd/api/server UserGamesModel
 //go:generate mockgen -destination authenticatormock.go  -package fixtures -mock_names Authenticator=AuthenticatorMock github.com/asankov/gira/cmd/api/server Authenticator
 //go:generate mockgen -destination renderer_mock.go  -package fixtures -mock_names Renderer=RendererMock github.com/asankov/gira/cmd/front-end/server Renderer
