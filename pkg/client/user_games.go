a4f621eb40f80d8400aeade530667c984a0539b0
asankov <a.sankov96@gmail.com>
2020-05-31 20:42:19 +0300

    Move `client/games.go::GetUserGames`
    to `client/user_games.go::GetUserGames`

---
+++ pkg/client/user_games.go
@@ -0,0 +1,35 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/asankov/gira/pkg/models"
+)
+
+// GetUserGames returns all the games for the given user.
+func (c *Client) GetUserGames(token string) ([]*models.Game, error) {
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/users/%s/games", c.addr, "TODO"), nil)
+	if err != nil {
+		return nil, fmt.Errorf("error while building HTTP request")
+	}
+	req.Header.Add("x-auth-token", token)
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, ErrFetchingGames
+	}
+	if res.StatusCode != http.StatusOK {
+		if res.StatusCode == http.StatusUnauthorized {
+			return nil, ErrNoAuthorization
+		}
+		return nil, ErrFetchingGames
+	}
+
+	var games []*models.Game
+	if err := json.NewDecoder(res.Body).Decode(&games); err != nil {
+		return nil, fmt.Errorf("error while decoidng body: %w", err)
+	}
+
+	return games, nil
+}
