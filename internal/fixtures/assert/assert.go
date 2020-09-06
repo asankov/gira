87fda7424fe66ca45b9acb45128e88c7e561a5bd
Anton Sankov <a.sankov96@gmail.com>
2020-09-07 02:31:59 +0300

    Add test for Change game status
    add internal `assert` package with assert helpers

---
+++ internal/fixtures/assert/assert.go
@@ -0,0 +1,24 @@
+package assert
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Redirect(t *testing.T, w *httptest.ResponseRecorder, uri string) {
+	if w.Code != http.StatusSeeOther {
+		t.Errorf("Got (%d) for status code, expected (%d)", w.Code, http.StatusSeeOther)
+	}
+
+	got, expected := w.Header().Get("Location"), uri
+	if got != expected {
+		t.Errorf("Got %s for Location header, expected %s", got, expected)
+	}
+}
+
+func StatusCode(t *testing.T, w *httptest.ResponseRecorder, statusCode int) {
+	if w.Code != statusCode {
+		t.Errorf("Got (%d) status code, expected (%d)", w.Code, statusCode)
+	}
+}
