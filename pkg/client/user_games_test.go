ef3f575537d9cb36408fcf4eda746c833f85f489
Anton Sankov <a.sankov96@gmail.com>
2020-09-12 01:46:03 +0300

    More tests for user games

--- pkg/client/user_games_test.go
+++ pkg/client/user_games_test.go
@@ -1,6 +1,7 @@
 package client_test
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 
@@ -132,6 +133,104 @@ func TestLinkGameToUserHTTPError(t *testing.T) {
 			cl := newClient(t, ts.URL)
 
 			_, err := cl.LinkGameToUser("12", token)
+			assert.Error(t, err, testCase.expectedErr)
+		})
+	}
+}
+
+func TestChangeGameStatus(t *testing.T) {
+	ts := fixtures.NewTestServer(t).
+		Path(fmt.Sprintf("/users/games/%s", game.ID)).
+		Method(http.MethodPatch).
+		Token(token).
+		Build()
+	defer ts.Close()
+
+	cl := newClient(t, ts.URL)
+
+	err := cl.ChangeGameStatus(game.ID, token, models.StatusDone)
+	assert.NoError(t, err)
+}
+
+func TestChangeGameStatusHTTPError(t *testing.T) {
+	testCases := []struct {
+		name         string
+		responseCode int
+		expectedErr  error
+	}{
+		{
+			name:         "Auth error",
+			responseCode: http.StatusUnauthorized,
+			expectedErr:  client.ErrNoAuthorization,
+		},
+		{
+			name:         "Other error",
+			responseCode: http.StatusInternalServerError,
+			expectedErr:  client.ErrChangingGameStatus,
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			ts := fixtures.NewTestServer(t).
+				Path(fmt.Sprintf("/users/games/%s", game.ID)).
+				Method(http.MethodPatch).
+				Token(token).
+				Return(testCase.responseCode).
+				Build()
+			defer ts.Close()
+
+			cl := newClient(t, ts.URL)
+
+			err := cl.ChangeGameStatus(game.ID, token, models.StatusTODO)
+			assert.Error(t, err, testCase.expectedErr)
+		})
+	}
+}
+
+func TestDeleteGame(t *testing.T) {
+	ts := fixtures.NewTestServer(t).
+		Path(fmt.Sprintf("/users/games/%s", game.ID)).
+		Method(http.MethodDelete).
+		Token(token).
+		Build()
+	defer ts.Close()
+
+	cl := newClient(t, ts.URL)
+
+	err := cl.DeleteUserGame(game.ID, token)
+	assert.NoError(t, err)
+}
+
+func TestDeleteGameHTTPError(t *testing.T) {
+	testCases := []struct {
+		name         string
+		responseCode int
+		expectedErr  error
+	}{
+		{
+			name:         "Auth error",
+			responseCode: http.StatusUnauthorized,
+			expectedErr:  client.ErrNoAuthorization,
+		},
+		{
+			name:         "Other error",
+			responseCode: http.StatusInternalServerError,
+			expectedErr:  client.ErrDeletingGame,
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			ts := fixtures.NewTestServer(t).
+				Path(fmt.Sprintf("/users/games/%s", game.ID)).
+				Method(http.MethodDelete).
+				Token(token).
+				Return(testCase.responseCode).
+				Build()
+			defer ts.Close()
+
+			cl := newClient(t, ts.URL)
+
+			err := cl.DeleteUserGame(game.ID, token)
 			assert.Error(t, err, testCase.expectedErr)
 		})
 	}
