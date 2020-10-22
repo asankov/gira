a9e14ca1d34838d76a039014a43e2e1ea2f77cb1
asankov <a.sankov96@gmail.com>
2020-10-22 22:06:38 +0300

    Tests for users and franchises clients

---
+++ pkg/client/users_test.go
@@ -0,0 +1,84 @@
+package client_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/asankov/gira/pkg/models"
+
+	"github.com/asankov/gira/internal/fixtures"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	user = &models.User{
+		ID:       "123",
+		Username: "test_user",
+		Email:    "test@mail.com",
+	}
+	userResponse = &models.UserResponse{
+		User: user,
+	}
+	userLoginResponse = &models.UserLoginResponse{
+		Token: token,
+	}
+)
+
+func TestGetUser(t *testing.T) {
+	ts := fixtures.NewTestServer(t).
+		Path("/users").
+		Token(token).
+		Data(userResponse).
+		Build()
+	defer ts.Close()
+
+	cl := newClient(t, ts.URL)
+
+	usr, err := cl.GetUser(token)
+	require.NoError(t, err)
+	require.Equal(t, usr, user)
+}
+
+func TestCreateUser(t *testing.T) {
+	ts := fixtures.NewTestServer(t).
+		Path("/users").
+		Method(http.MethodPost).
+		Data(user).
+		Build()
+	defer ts.Close()
+
+	cl := newClient(t, ts.URL)
+
+	usr, err := cl.CreateUser(user)
+	require.NoError(t, err)
+	require.Equal(t, usr, user)
+}
+
+func TestLogin(t *testing.T) {
+	ts := fixtures.NewTestServer(t).
+		Path("/users/login").
+		Method(http.MethodPost).
+		Data(userLoginResponse).
+		Build()
+	defer ts.Close()
+
+	cl := newClient(t, ts.URL)
+
+	usrLoginResponse, err := cl.LoginUser(user)
+	require.NoError(t, err)
+	require.Equal(t, usrLoginResponse, userLoginResponse)
+}
+
+func TestLogout(t *testing.T) {
+	ts := fixtures.NewTestServer(t).
+		Method(http.MethodPost).
+		Path("/users/logout").
+		Token(token).
+		Build()
+	defer ts.Close()
+
+	cl := newClient(t, ts.URL)
+
+	err := cl.LogoutUser(token)
+	require.NoError(t, err)
+}
