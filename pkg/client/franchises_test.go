d3fef6395a68cc2ff88b2a1471a05bb85d0bd367
asankov <a.sankov96@gmail.com>
2020-10-22 22:06:38 +0300

    Tests for users and franchises clients

---
+++ pkg/client/franchises_test.go
@@ -0,0 +1,56 @@
+package client_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/asankov/gira/internal/fixtures"
+	"github.com/asankov/gira/pkg/client"
+	"github.com/asankov/gira/pkg/models"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	franchise = &models.Franchise{
+		ID:   "1",
+		Name: "Batman",
+	}
+	franchises         = []*models.Franchise{franchise}
+	franchisesResponse = &models.FranchisesResponse{
+		Franchises: franchises,
+	}
+)
+
+func TestFranchisesGet(t *testing.T) {
+	ts := fixtures.NewTestServer(t).
+		Path("/franchises").
+		Token(token).
+		Method(http.MethodGet).
+		Data(franchisesResponse).
+		Build()
+	defer ts.Close()
+
+	cl := newClient(t, ts.URL)
+
+	fr, err := cl.GetFranchises(token)
+	require.NoError(t, err)
+	require.Equal(t, franchises, fr)
+}
+
+func TestFranchisesCreate(t *testing.T) {
+	ts := fixtures.NewTestServer(t).
+		Path("/franchises").
+		Token(token).
+		Method(http.MethodPost).
+		Data(franchise).
+		Build()
+	defer ts.Close()
+
+	cl := newClient(t, ts.URL)
+
+	frResponse, err := cl.CreateFranchise(&client.CreateFranchiseRequest{
+		Name: "AC",
+	}, token)
+	require.NoError(t, err)
+	require.Equal(t, franchise, frResponse.Franchise)
+}
