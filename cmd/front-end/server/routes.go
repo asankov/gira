145f97c01f63a2fdb254879136dfdf512d9913d8
Anton Sankov <a.sankov96@gmail.com>
2020-10-31 15:54:28 +0200

    Enabled Session for all routes
    and remove `dynamicMiddleware`, because otherwise we get errors on FE
    and because there is no good reason to not enable session everywhere

--- cmd/front-end/server/routes.go
+++ cmd/front-end/server/routes.go
@@ -13,22 +13,20 @@ import (
 func (s *Server) routes() http.Handler {
 	r := mux.NewRouter()
 
-	dynamicMiddleware := alice.New(s.Session.Enable)
-
 	r.HandleFunc("/", s.handleHome()).Methods(http.MethodGet)
-	r.Handle("/games", dynamicMiddleware.Then(s.requireLogin(s.handleGamesGet()))).Methods(http.MethodGet)
-	r.Handle("/games/add", dynamicMiddleware.Then(s.requireLogin(s.handleGamesAdd()))).Methods(http.MethodGet)
-	r.Handle("/games/add", dynamicMiddleware.Then(s.requireLogin(s.handleGamesAddPost()))).Methods(http.MethodPost)
-	r.Handle("/games/new", dynamicMiddleware.Then(s.requireLogin(s.handleGameCreateView()))).Methods(http.MethodGet)
-	r.Handle("/games", dynamicMiddleware.Then(s.requireLogin(s.handleGameCreate()))).Methods(http.MethodPost)
-	r.Handle("/games/status", dynamicMiddleware.Then(s.requireLogin(s.handleGamesChangeStatus()))).Methods(http.MethodPost)
-	r.Handle("/games/progress", dynamicMiddleware.Then(s.requireLogin(s.handleGamesChangeProgress()))).Methods(http.MethodPost)
-	r.Handle("/games/delete", dynamicMiddleware.Then(s.requireLogin(s.handleGamesDelete()))).Methods(http.MethodPost)
+	r.Handle("/games", s.requireLogin(s.handleGamesGet())).Methods(http.MethodGet)
+	r.Handle("/games/add", s.requireLogin(s.handleGamesAdd())).Methods(http.MethodGet)
+	r.Handle("/games/add", s.requireLogin(s.handleGamesAddPost())).Methods(http.MethodPost)
+	r.Handle("/games/new", s.requireLogin(s.handleGameCreateView())).Methods(http.MethodGet)
+	r.Handle("/games", s.requireLogin(s.handleGameCreate())).Methods(http.MethodPost)
+	r.Handle("/games/status", s.requireLogin(s.handleGamesChangeStatus())).Methods(http.MethodPost)
+	r.Handle("/games/progress", s.requireLogin(s.handleGamesChangeProgress())).Methods(http.MethodPost)
+	r.Handle("/games/delete", s.requireLogin(s.handleGamesDelete())).Methods(http.MethodPost)
 
-	r.Handle("/franchises/add", dynamicMiddleware.Then(s.requireLogin(s.handleFranchisesAddPost()))).Methods(http.MethodPost)
+	r.Handle("/franchises/add", s.requireLogin(s.handleFranchisesAddPost())).Methods(http.MethodPost)
 
-	r.Handle("/users/signup", dynamicMiddleware.Then(s.handleUserSignupForm())).Methods(http.MethodGet)
-	r.Handle("/users/create", dynamicMiddleware.Then(s.handleUserSignup())).Methods(http.MethodPost)
+	r.Handle("/users/signup", s.handleUserSignupForm()).Methods(http.MethodGet)
+	r.Handle("/users/create", s.handleUserSignup()).Methods(http.MethodPost)
 
 	r.Handle("/users/login", s.handleUserLoginForm()).Methods(http.MethodGet)
 	r.Handle("/users/login", s.handleUserLogin()).Methods(http.MethodPost)
@@ -37,6 +35,6 @@ func (s *Server) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static", fileServer))
 
-	standartMiddleware := alice.New(middleware.RecoverPanic(s.Log), middleware.LogRequest(s.Log), s.secureHeaders)
+	standartMiddleware := alice.New(middleware.RecoverPanic(s.Log), middleware.LogRequest(s.Log), s.secureHeaders, s.Session.Enable)
 	return standartMiddleware.Then(r)
 }
