cc9d728909cd022a4ad366e59d3531f57c222e2a
Anton Sankov <a.sankov96@gmail.com>
2020-11-01 03:42:21 +0200

    Remove references to front-end and put TODOs for later

--- internal/fixtures/generated_fixtures.go
+++ internal/fixtures/generated_fixtures.go
@@ -5,5 +5,3 @@ package fixtures
 //go:generate mockgen -destination user_games_model_mock.go  -package fixtures -mock_names UserGamesModel=UserGamesModelMock github.com/asankov/gira/cmd/api/server UserGamesModel
 //go:generate mockgen -destination franchises_model_mock.go  -package fixtures -mock_names FranchiseModel=FranchiseModelMock github.com/asankov/gira/cmd/api/server FranchiseModel
 //go:generate mockgen -destination authenticatormock.go  -package fixtures -mock_names Authenticator=AuthenticatorMock github.com/asankov/gira/cmd/api/server Authenticator
-//go:generate mockgen -destination renderer_mock.go  -package fixtures -mock_names Renderer=RendererMock github.com/asankov/gira/cmd/front-end/server Renderer
-//go:generate mockgen -destination api_client_mock.go  -package fixtures -mock_names APIClient=APIClientMock github.com/asankov/gira/cmd/front-end/server APIClient
