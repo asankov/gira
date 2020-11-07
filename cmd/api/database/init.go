939754d06508d0c282c8dd7bde4512571d3b6655
Anton Sankov <a.sankov96@gmail.com>
2020-11-07 20:34:31 +0200

    Add possibility to customize SQL directory

--- cmd/api/database/init.go
+++ cmd/api/database/init.go
@@ -3,12 +3,18 @@ package database
 import "github.com/pressly/goose"
 
 const (
-	upCommand    = "up"
-	sqlDirectory = "./sql"
+	upCommand = "up"
 )
 
 // Init runs the migrations, needed to have a working Gira DB.
+// It gets the SQL files from the default `./sql` directory.
 func Init(opts *DBOptions) error {
+	return InitFromDirectory(opts, "./sql")
+}
+
+// InitFromDirectory runs the migrations, needed to have a working Gira DB.
+// It gets the SQL files from the provided directory.
+func InitFromDirectory(opts *DBOptions, sqlDirectory string) error {
 	db, err := NewDB(opts)
 	if err != nil {
 		return err
